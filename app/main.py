8449e911226763fc056d0575160cec7ceed452da
Appaz Kydyraliuulu <akydyraliuulu@gmail.com>
2026-07-15 19:34:36 +0900

    add round calculate_washing_price

--- app/main.py
+++ app/main.py
@@ -44,10 +44,10 @@ class CarWashStation:
         :param car: Car
         :return: float
         """
-        return (car.comfort_class
+        return round((car.comfort_class
                 * (self.clean_power - car.clean_mark)
                 * self.average_rating
-                / self.distance_from_city_center)
+                / self.distance_from_city_center), 1)
 
     def wash_single_car(self, car: Car) -> None:
         """
